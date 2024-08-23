ba6f4d0b80997858ab2d8d371b7f24dbc38275ff
acriollo <acriollo@iuvity.com>
2024-08-22 21:45:47 -0500

    First commit

---
+++ src/calendar/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark  mb-4 px-4">
+      <span className="navbar-brand"> 
+        <i className="fas fa-calendar-alt" ></i>
+        &nbsp;
+        Amada C
+      </span>
+
+      <button>
+        <i className="fas fas-sign" ></i>
+        <span>Salir</span>
+        </button>
+    </div>
+  )
+}
+
